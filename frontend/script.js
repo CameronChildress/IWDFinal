a6a5ca12a95385a0a73279483a2534cdc8505a13
CameronChildress <cameronchildress4@gmail.com>
2021-08-30 08:11:51 -0600

    Changed Colors on graph

--- frontend/script.js
+++ frontend/script.js
@@ -30,29 +30,29 @@ updateGraphs = () => {
 
 updateGraph1 = (data1) => {
     colorText(ctx1, "#fff", "uh", 0, 40);
-    colorRect(ctx1, "#0f0", 0, 40, data1[0] * 50, 100);
+    colorRect(ctx1, "#b09e99", 0, 40, data1[0] * 50, 100);
     colorText(ctx1, "#fff", "wha-", 0, 180);
-    colorRect(ctx1, "#f00", 0, 180, data1[1] * 50, 100);
+    colorRect(ctx1, "#fad4c0", 0, 180, data1[1] * 50, 100);
     colorText(ctx1, "#fff", "whuhhh", 0, 320);
-    colorRect(ctx1, "#00f", 0, 320, data1[2] * 50, 100);
+    colorRect(ctx1, "#64b6ac", 0, 320, data1[2] * 50, 100);
     colorText(ctx1, "#fff", "doo wahhhh", 0, 460);
-    colorRect(ctx1, "#ff0", 0, 460, data1[3] * 50, 100);
+    colorRect(ctx1, "#c0fdfb", 0, 460, data1[3] * 50, 100);
 }
 
 updateGraph2 = (data2) => {
     colorText(ctx2, "#fff", "What the dog doin?", 0, 40);
-    colorRect(ctx2, "#0f0", 0, 40, data2[0] * 50, 100);
+    colorRect(ctx2, "#b09e99", 0, 40, data2[0] * 50, 100);
     colorText(ctx2, "#fff", "How the dog doin?", 0, 180);
-    colorRect(ctx2, "#f00", 0, 180, data2[1] * 50, 100);
+    colorRect(ctx2, "#fad4c0", 0, 180, data2[1] * 50, 100);
 }
 
 updateGraph3 = (data3) => {
     colorText(ctx3, "#fff", "Yes", 0, 40);
-    colorRect(ctx3, "#0f0", 0, 40, data3[0] * 50, 100);
+    colorRect(ctx3, "#b09e99", 0, 40, data3[0] * 50, 100);
     colorText(ctx3, "#fff", "No", 0, 180);
-    colorRect(ctx3, "#f00", 0, 180, data3[1] * 50, 100);
+    colorRect(ctx3, "#fad4c0", 0, 180, data3[1] * 50, 100);
     colorText(ctx3, "#fff", "*liard sounds*", 0, 320);
-    colorRect(ctx3, "#00f", 0, 320, data3[2] * 50, 100);
+    colorRect(ctx3, "#64b6ac", 0, 320, data3[2] * 50, 100);
 }
 
 colorRect = (ctx, color, x, y, width, height) => {
